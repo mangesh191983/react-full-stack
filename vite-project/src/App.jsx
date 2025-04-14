236f67289d8b9a6eaacf5f4e94c384eed956db18
unknown <mangesh.gharate@trigyn.com>
2025-04-14 11:07:05 +0530

    Navbar

--- vite-project/src/App.jsx
+++ vite-project/src/App.jsx
@@ -4,28 +4,37 @@ import './App.css'
 import Homepage from './pages/Homepage'
 import News from './pages/News';
 import Articles from './pages/Articles';
+import Navbar from './Navbar';
+import Layout from './Layout';
+import { Children } from 'react';
 
 const routes = [{ 
   path: '/',
-  element: <Homepage/>
-},
-{ 
-  path: '/articles',
-  element: <Articles/>
-},
-{ 
-  path: '/news',
-  element: <News/>
-}
+  element: <Layout/>,
+  children: [{ 
+    path: '/',
+    element: <Homepage/>
+  },
+  { 
+    path: '/articles',
+    element: <Articles/>
+  },
+  { 
+    path: '/news',
+    element: <News/>
+  }]  
+}];
 
-];
+
 
 const router = createBrowserRouter(routes);
 
 
 function App() {
   return (
+    <>    
     <RouterProvider router= {router}></RouterProvider>
+    </>
   );
 }
 
---
+++ vite-project/src/Navbar.jsx
@@ -0,0 +1,19 @@
+import { Link } from 'react-router-dom'
+
+export default function Navbar() {
+    return (
+        <nav>
+            <ul>
+                <li>
+                    <Link to ='/'>Home</Link>
+                </li>
+                <li>
+                    <Link to ='/articles'>Articles</Link>
+                </li>
+                <li>
+                    <Link to ='/news'>News</Link>
+                </li>
+            </ul>
+        </nav>        
+    );
+}
