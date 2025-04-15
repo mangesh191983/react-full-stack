1a76a78da348fb08fa7b72b55a2c819414325e8e
unknown <mangesh.gharate@trigyn.com>
2025-04-15 11:51:24 +0530

    Nodemon installed, autoscript done, article comment api

--- back-end/src/server.js
+++ back-end/src/server.js
@@ -6,15 +6,31 @@ const app = express();
 app.use(express.json());
 
 const articleVote = [
-    {name:"learn-react", vote:0},
-    {name:"learn-node", vote:0},
-    {name:"mongodb", vote:0}
+    {name:"learn-react", vote:0, comment:[]},
+    {name:"learn-node", vote:0, comment:[]},
+    {name:"mongodb", vote:0, comment:[]}
 ]
 
 app.post('/api/article/:name/upvote',function(req,res){
     const article = articleVote.find(a => a.name ===req.params.name);
     article.vote++;
-    res.send("Article "+article.name+" total votes are "+article.vote);
+    res.json(article);
+});
+
+app.post('/api/article/:name/comment',function(req,res){
+    const { name } = req.params;
+    const { postedBy, text } = req.body   
+
+    const article = articleVote.find(a => a.name === name);
+    article.comment.push ({postedBy:postedBy,text:text});
+
+    res.json(article);
+});
+
+app.post('/api/article/:name/upvote',function(req,res){
+    const article = articleVote.find(a => a.name ===req.params.name);
+    article.vote++;
+    res.json(article);
 });
 
 app.listen(8000,function(){
